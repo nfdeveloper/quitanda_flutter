6f8e7c3d1a680105d4ff53429bdd0c45e3337d5c
nfdeveloper04 <nfdeveloper04@gmail.com>
2023-03-29 00:49:41 -0300

    Projeto Quitanda. Criando Tela de Login

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,155 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:quitanda/src/auth/components/custom_text_field.dart';
+import 'package:quitanda/src/config/custom_colors.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    //Recuperando o tamanho da tela
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Nomedo App
+                   Text.rich(TextSpan(
+                      style: const TextStyle(
+                        fontSize: 40,
+                      ),
+                      children: [
+                        const TextSpan(
+                            text: 'Green',
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold)),
+                        TextSpan(
+                            text: 'grocer', 
+                            style: TextStyle(
+                              color: CustomColors.customContrastColor,                        )
+                        )
+                      ])),
+              
+                  // Animação Categorias
+                  SizedBox(
+                    height: 30,
+                    child: DefaultTextStyle(
+                      style: const TextStyle(
+                        fontSize: 25
+                      ),
+                      child: AnimatedTextKit(
+                        pause: Duration.zero,
+                        repeatForever: true,
+                        animatedTexts: [
+                          FadeAnimatedText('Frutas'),
+                          FadeAnimatedText('Verduras'),
+                          FadeAnimatedText('Legumes'),
+                          FadeAnimatedText('Carnes'),
+                          FadeAnimatedText('Cereais'),
+                          FadeAnimatedText('Laticíneos'),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )),
+              
+              // => Formulário
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      //Email
+                      CustomTextField(
+                        icon: Icons.email,
+                        label: 'Email',
+                      ),
+              
+                      //Senha
+                      CustomTextField(
+                        icon: Icons.lock,
+                        label: 'Senha',
+                        isSecret: true,
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18))),
+                          onPressed: () {},
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Esqueceu a senha?',
+                            style: TextStyle(color: CustomColors.customContrastColor),
+                          ),
+                        ),
+                      ),
+              
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Divider(
+                                    color: Colors.grey.withAlpha(90),
+                                    thickness: 2)),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: Text('Ou'),
+                            ),
+                            Expanded(
+                                child: Divider(
+                                    color: Colors.grey.withAlpha(90),
+                                    thickness: 2)),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18)),
+                              side:
+                                  const BorderSide(width: 2, color: Colors.green)),
+                          onPressed: () {},
+                          child: const Text(
+                            'Criar Conta',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      )
+                    ]),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
